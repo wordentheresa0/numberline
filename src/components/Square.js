3b5203eeda8b832d2d2b923fffe19d577166a97f
Will Brandt <willbrandt@Wills-MacBook-Air.local>
2024-01-17 12:44:08 +0000

    lfg

--- src/components/Square.js
+++ src/components/Square.js
@@ -5,11 +5,20 @@ const Square = ({ text, isCurrent, onClick }) => {
   const [backgroundImage, setBackgroundImage] = useState('');
 
   useEffect(() => {
-    setBackgroundImage(isCurrent
-      ? 'https://i.pinimg.com/564x/33/16/c2/3316c2cb0b978dadfa1fb391fc454e48.jpg'
-      : 'https://www.americanoceans.org/wp-content/uploads/2023/06/spongebob-squarepants-1024x671.jpeg'
-    );
+    let backgroundImageUrl;
+  
+    if (text === 30) {
+      backgroundImageUrl = "https://wallpapers.com/images/high/sad-spongebob-n06og83jcwiics4r.webp";
+    } else if (isCurrent) {
+      backgroundImageUrl = "'https://i.pinimg.com/564x/33/16/c2/3316c2cb0b978dadfa1fb391fc454e48.jpg'";
+    } else {
+      backgroundImageUrl = 'https://www.americanoceans.org/wp-content/uploads/2023/06/spongebob-squarepants-1024x671.jpeg';
+    }
+  
+    setBackgroundImage(backgroundImageUrl);
+    // eslint-disable-next-line
   }, [isCurrent]);
+
 
   const squareStyle = {
     backgroundImage: `url(${backgroundImage})`,
